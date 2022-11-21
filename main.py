2b552ee6e934f899eb4cb6a9e9f1cd7da0133ba6
Ismail <ismail.guersoez@barely.digital>
2022-11-21 09:41:43 +0100

    Added Loading Model if Checkpoints exist

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import os.path
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 import keras
+import keras.utils as ut
 from typing import ClassVar, Callable, Union, List
 from loss_functions.emd import earth_mover_distance, self_guided_earth_mover_distance, \
     approximate_earth_mover_distance, GroundDistanceManager, EmdWeightHeadStart
@@ -74,31 +75,57 @@ def resblock(x, filters, strides):
 
 
 if __name__ == '__main__':
-    # Getting the Filepath of the Images and the Labels and concat it in Panda Array(Series) aka Dataframe called images
+    ## Getting the Filepath of the Images and the Labels and concat it in Panda Array(Series) aka Dataframe called images
     db = "imdb"
-    image_dir = Path('./{}_crop'.format(db))
+    #image_dir = Path('./{}_crop'.format(db))
+    image_dir = Path('./age_prediction')
     mat_path = './{}_crop/{}.mat'.format(db, db)
     filepaths = pd.Series(list(image_dir.glob(r'**/*.jpg')), name="Filepath").astype(str)
-    ages = pd.Series(get_age(mat_path, db), name="Age").astype(int)
-    #ages = pd.Series(filepaths.apply(lambda x: os.path.split(os.path.split(x)[0])[1]), name="Age").astype(int)
+    #ages = pd.Series(get_age(mat_path, db), name="Age").astype(int)
+    #ages = pd.Series(get_meta(mat_path, db), name="Age").astype(int)
+    ages = pd.Series(filepaths.apply(lambda x: os.path.split(os.path.split(x)[0])[1]), name="Age").astype(int)
     images = pd.concat([filepaths, ages], axis=1).sample(frac=1.0, random_state=1).reset_index(drop=True)
-    #print(images)
-    #exit()
 
-    # use only 10000 images to speed up training time
-    image_df = images.sample(10000, random_state=1).reset_index(drop=True)
-    # splitting images into train and test
+    ## use only 10000 images to speed up training time
+    image_df = images.sample(10000, random_state=np.random.randint(1000)).reset_index(drop=True)
+    test_image_df = images.sample(1, random_state=np.random.randint(1000)).reset_index(drop=True)
+
+    ## splitting images into train and test
     train_df, test_df = train_test_split(image_df, train_size=0.7, shuffle=True, random_state=1)
 
-    # Defining the ImageDataGenerator and what Preprocessing should be done to the images
+    ## Defining the ImageDataGenerator and what Preprocessing should be done to the images
+    ## normalize each image in image_df to have mean of 0 and deviation of 1
     train_generator = tf.keras.preprocessing.image.ImageDataGenerator(
-        rescale=1. / 255,
-        validation_split=0.2
+        #rescale=1. / 255,
+        validation_split=0.2,
+        featurewise_center=True,
+        featurewise_std_normalization=True
     )
 
     test_generator = tf.keras.preprocessing.image.ImageDataGenerator(
-        rescale=1. / 255,
+        #rescale=1. / 255,
+        featurewise_center=True,
+        featurewise_std_normalization=True
     )
+
+
+    imgs = []
+    for i in train_df["Filepath"]:
+        image = tf.keras.preprocessing.image.img_to_array(keras.utils.load_img(i, target_size=(244,244)))
+        #image = np.resize(image, (244, 244, 3))
+        imgs.append(image)
+    imgs = np.stack(imgs)
+    print(imgs.shape)
+    train_generator.fit(imgs)
+
+    imgs = []
+    for i in test_df["Filepath"]:
+        image = tf.keras.preprocessing.image.img_to_array(ut.load_img(i, target_size=(244,244)))
+        #image = np.resize(image, (244, 244, 3))
+        imgs.append(image)
+    imgs = np.stack(imgs)
+    print(imgs.shape)
+    test_generator.fit(imgs)
 
     # Getting the Images from Filepath
     train_images = train_generator.flow_from_dataframe(
@@ -108,7 +135,7 @@ if __name__ == '__main__':
         target_size=(224, 224),
         color_mode="rgb",
         class_mode="raw",
-        batch_size=32,
+        batch_size=54,
         shuffle=True,
         seed=42,
         subset="training"
@@ -121,7 +148,7 @@ if __name__ == '__main__':
         target_size=(224, 224),
         color_mode="rgb",
         class_mode="raw",
-        batch_size=32,
+        batch_size=64,
         shuffle=True,
         seed=42,
         subset="validation"
@@ -134,40 +161,38 @@ if __name__ == '__main__':
         target_size=(224, 224),
         color_mode="rgb",
         class_mode="raw",
-        batch_size=32,
+        batch_size=64,
         shuffle=False
     )
 
-    # img1 = train_df['Filepath'].iloc[0]
-    # img = pil.open(img1)
-    # img.show()
-    # exit()
-
-    NrClasses = 100
-
+    ## Defining the different Models to trtain
     models = ['regressorMSE', 'classificatorXE', 'classificatorEMD']
-
-    input = tf.keras.Input(shape=(224, 224, 3))
-    out0 = tf.keras.layers.Conv2D(filters=64, kernel_size=(7, 7), strides=(2, 2), padding="same")(input)
-    out1 = resblock(out0, filters=64, strides=0)
-    out2 = resblock(out1, filters=64, strides=0)
-    out3 = resblock(out2, filters=128, strides=1)
-    out4 = resblock(out3, filters=128, strides=0)
-    out5 = resblock(out4, filters=256, strides=1)
-    out6 = resblock(out5, filters=256, strides=0)
-    out7 = resblock(out6, filters=512, strides=1)
-    out8 = resblock(out7, filters=512, strides=0)
-    out = tf.keras.layers.GlobalAveragePooling2D()(out8)
 
     userInput = builtins.input(
         'Please enter which Model you want to train:\n 1:regressorMSE \n 2:classificatorXE \n 3:classificatorEMD \n 4:End Training \n')
-
     while userInput != 4:
         match userInput:
             case "1":
-                checkpoint_path = "./checkpoints/regressorMSE/regressorMSE"
-                output = tf.keras.layers.Dense(1, activation="relu")(out)
-                model = keras.Model(input, output)
+                checkpoint_path = "./checkpoints/regressorMSE/regressorMSE.ckpt"
+                model_path = "./checkpoints/regressorMSE/regressorMSE.ckpt"
+                checkpoint_path_check = "./checkpoints/regressorMSE/checkpoint"
+                if os.path.exists(checkpoint_path):
+                    print("Model detected. Loading Model...")
+                    model = keras.models.load_model(checkpoint_path)
+                else:
+                    input = tf.keras.Input(shape=(224, 224, 3))
+                    out0 = tf.keras.layers.Conv2D(filters=64, kernel_size=(7, 7), strides=(2, 2), padding="same")(input)
+                    out1 = resblock(out0, filters=64, strides=0)
+                    out2 = resblock(out1, filters=64, strides=0)
+                    out3 = resblock(out2, filters=128, strides=1)
+                    out4 = resblock(out3, filters=128, strides=0)
+                    out5 = resblock(out4, filters=256, strides=1)
+                    out6 = resblock(out5, filters=256, strides=0)
+                    out7 = resblock(out6, filters=512, strides=1)
+                    out8 = resblock(out7, filters=512, strides=0)
+                    out = tf.keras.layers.GlobalAveragePooling2D()(out8)
+                    output = tf.keras.layers.Dense(1, activation="relu")(out)
+                    model = keras.Model(input, output)
 
                 model.compile(
                     optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
@@ -175,18 +200,43 @@ if __name__ == '__main__':
                 )
 
             case "2":
-                checkpoint_path = "./checkpoints/classificatorXE/classificatorXE"
-                output = tf.keras.layers.Dense(100, activation="relu")(out)
+                checkpoint_path = "./checkpoints/classificatorXE/classificatorXE.ckpt"
+                checkpoint_path_check = "./checkpoints/classificatorXE/checkpoint"
+
+                input = tf.keras.Input(shape=(224, 224, 3))
+                out0 = tf.keras.layers.Conv2D(filters=64, kernel_size=(7, 7), strides=(2, 2), padding="same")(input)
+                out1 = resblock(out0, filters=64, strides=0)
+                out2 = resblock(out1, filters=64, strides=0)
+                out3 = resblock(out2, filters=128, strides=1)
+                out4 = resblock(out3, filters=128, strides=0)
+                out5 = resblock(out4, filters=256, strides=1)
+                out6 = resblock(out5, filters=256, strides=0)
+                out7 = resblock(out6, filters=512, strides=1)
+                out8 = resblock(out7, filters=512, strides=0)
+                out = tf.keras.layers.GlobalAveragePooling2D()(out8)
+                output = tf.keras.layers.Dense(10, activation="relu")(out)
                 output = tf.keras.layers.Softmax()(output)
                 model = keras.Model(input, output)
                 model.compile(
                     optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
-                    loss=tf.keras.losses.SparseCategoricalCrossentropy()
+                    loss=tf.keras.losses.CategoricalCrossentropy()
                 )
 
-
             case "3":
-                checkpoint_path = "./checkpoints/classificatorEMD/classificatorEMD"
+                checkpoint_path = "./checkpoints/classificatorEMD/classificatorEMD.ckpt"
+                checkpoint_path_check = "./checkpoints/classificatorEMD/checkpoint"
+
+                input = tf.keras.Input(shape=(224, 224, 3))
+                out0 = tf.keras.layers.Conv2D(filters=64, kernel_size=(7, 7), strides=(2, 2), padding="same")(input)
+                out1 = resblock(out0, filters=64, strides=0)
+                out2 = resblock(out1, filters=64, strides=0)
+                out3 = resblock(out2, filters=128, strides=1)
+                out4 = resblock(out3, filters=128, strides=0)
+                out5 = resblock(out4, filters=256, strides=1)
+                out6 = resblock(out5, filters=256, strides=0)
+                out7 = resblock(out6, filters=512, strides=1)
+                out8 = resblock(out7, filters=512, strides=0)
+                out = tf.keras.layers.GlobalAveragePooling2D()(out8)
                 output = tf.keras.layers.Dense(100, activation="relu")(out)
                 output = tf.keras.layers.Softmax()(output)
                 model = keras.Model(input, output)
@@ -194,18 +244,24 @@ if __name__ == '__main__':
                     optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
                     loss=earth_mover_distance()
                 )
-
+            case "4":
+                break
             case _:
                 print("No Model selected. Please try again.")
                 userInput = builtins.input(
                     'Please enter which Model you want to train:\n 1:regressorMSE \n 2:classificatorXE \n 3:classificatorEMD \n 4:End Training  \n')
                 continue
 
-        if os.path.isfile(checkpoint_path):
+        if os.path.isfile(checkpoint_path_check):
             print("Checkpoint detected. Loading Weights...")
-            model.load_weights(filepath=checkpoint_path)
+            #model.load_weights(filepath=checkpoint_path)
+
+            #model = keras.models.load_model(checkpoint_path)
         else:
             print("No Checkpoints detected. Starting new Training")
+        #model.load_weights(filepath=checkpoint_path)
+        print(model.trainable_weights)
+        #exit()
         history = model.fit(
             x=train_images,
             validation_data=val_images,
@@ -214,11 +270,15 @@ if __name__ == '__main__':
                 tf.keras.callbacks.EarlyStopping(
                     monitor="val_loss",
                     patience=5,
-                    restore_best_weights=True
+                    restore_best_weights=True,
+                    mode="min"
                 ),
                 tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
-                                                   save_weights_only=True,
-                                                   verbose=1)
+                                                   save_weights_only=False,
+                                                   mode="min",
+                                                   verbose=1,
+                                                   save_best_only=True,
+                                                   initial_value_threshold=200)
             ]
         )
         print("Training ended. Now to Testing")
@@ -228,7 +288,7 @@ if __name__ == '__main__':
 
         print("Testing finished. Results are: ")
 
-        rmse = np.sqrt(model.evaluate(test_images, verbose=0))
+        rmse = np.sqrt(model.evaluate(test_images, verbose=1))
         print("Test RMSE: {:.5f}".format(rmse))
 
         r2 = r2_score(actual_ages, predicted_ages)
@@ -239,7 +299,7 @@ if __name__ == '__main__':
         builtins.input("Press something to continue...")
 
         userInput = builtins.input(
-            'Training finished. If you want to continue, please enter which Model you want to train:\n 1:regressorMSE \n 2:classificatorXE \n 3:classificatorEMD \n 4:End Training \n')
+            'Training finished. If you want to continue, please enter which Model you want to train:\n 1:regressorMSE \n 2:classificatorXE \n 3:classificatorEMD \n 4:End Program \n')
 
     print("Program ended")
     exit()
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
+import keras.preprocessing.image
 from scipy.io import loadmat
 from datetime import datetime
+import numpy as np
 import os
+import keras.utils as ut
 
 
 def calc_age(taken, dob):
@@ -15,13 +18,16 @@ def calc_age(taken, dob):
 
 def get_meta(mat_path, db):
     meta = loadmat(mat_path)
-
     full_path = meta[db][0, 0]["full_path"][0]
     dob = meta[db][0, 0]["dob"][0]  # Matlab serial date number
     gender = meta[db][0, 0]["gender"][0]
     photo_taken = meta[db][0, 0]["photo_taken"][0]  # year
     face_score = meta[db][0, 0]["face_score"][0]
     second_face_score = meta[db][0, 0]["second_face_score"][0]
+    meta = meta[meta['face_score1'] != '-inf']
+    meta = meta[meta['face_score2'] == 'nan']
+    meta = meta.drop(['face_score1', 'face_score2'], axis=1)
+    meta = meta.sample(frac=1)
     age = [calc_age(photo_taken[i], dob[i]) for i in range(len(dob))]
 
     return full_path, dob, gender, photo_taken, face_score, second_face_score, age
@@ -37,5 +43,8 @@ def get_age(mat_path, db):
 
 def load_data(mat_path):
     d = loadmat(mat_path)
-
     return d["image"], d["gender"][0], d["age"][0], d["db"][0], d["img_size"][0, 0], d["min_score"][0, 0]
+
+def load_image(filepath):
+    test_image = ut.load_img(filepath, target_size=(244,244))
+    test_image = keras.preprocessing.image.img_to_array()
